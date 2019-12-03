16d3d2658f2e6a15d379f9794cf777057dc736f4
Alex M <alexm@dcode.com.au>
2019-12-04 10:29:57 +1100

    add sync command

---
+++ src/Commands/SyncFiles.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace DcodeGroup\FileMan\Commands;
+
+use DcodeGroup\FileMan\Services\FolderService;
+use Illuminate\Console\Command;
+
+class SyncFiles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fileman:sync';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sync your files with an S3 Bucket';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $bucketName = config('filesystems.disks.s3.bucket');
+        $this->info("Please wait indexing bucket {$bucketName}...");
+        FolderService::sync();
+        $this->info("Sync complete.");
+    }
+}
