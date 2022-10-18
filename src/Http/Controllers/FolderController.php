4bf540a959896feccfa02be1b7e1a7d9d5f2704e
Nathan Rzepecki <nathan@lionslair.net.au>
2022-10-18 16:00:46 +0800

    Cleanup the folder controller

--- src/Http/Controllers/FolderController.php
+++ src/Http/Controllers/FolderController.php
@@ -5,64 +5,45 @@ namespace DcodeGroup\Fileman\Http\Controllers;
 use DcodeGroup\Fileman\Http\Requests\FolderRequest;
 use DcodeGroup\Fileman\Models\Folder;
 use DcodeGroup\Fileman\Services\FolderService;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class FolderController extends BaseController
 {
-    /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function index(Folder $folder = null)
+    public function index(Folder $folder = null): View
     {
         if (!$folder) {
             $folder = Folder::getRoot();
         }
 
-        $directory = FolderService::getDirectoryStructure();
-        $path = $folder->getPath();
-        $files = $folder->files;
-
         return view('fileman::index')
             ->with([
                 'folder' => $folder,
-                'directory' => $directory,
-                'path' => $path,
-                'files' => $files
+                'directory' => FolderService::getDirectoryStructure(),
+                'path' =>  $folder->getPath(),
+                'files' => $folder->files
             ]);
     }
 
-    /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function create(Folder $parent)
+    public function create(Folder $parent): View
     {
-        $method = 'post';
-        $action = route('fileman.folder.store', $parent);
-        $directory = FolderService::getDirectoryStructure();
-        $path = $parent->getPath();
-
         return view('fileman::folder.edit')
             ->with([
-                'directory' => $directory,
-                'path' => $path,
-                'method' => $method,
-                'action' => $action,
+                'directory' => FolderService::getDirectoryStructure(),
+                'path' => $parent->getPath(),
+                'method' => 'post',
+                'action' => route('fileman.folder.store', $parent),
                 'parent' => $parent,
             ]);
     }
 
-    /**
-     * @param  FolderRequest  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function store(FolderRequest $request, Folder $parent)
+    public function store(FolderRequest $request, Folder $parent): RedirectResponse
     {
-        $folder = Folder::create([
-            'name' => request()->input('name'),
-            'parent_id' => $parent->id,
-        ]);
-
         return redirect()
-            ->route('fileman.folder.index', $folder);
+            ->route('fileman.folder.index', Folder::create([
+                'name' => request()->input('name'),
+                'parent_id' => $parent->id,
+            ]));
     }
 }
