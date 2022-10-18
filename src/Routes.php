47fbf8620fb5c36b58835cf3ed9891c3182eb5a2
Nathan Rzepecki <nathan@lionslair.net.au>
2022-10-18 15:53:03 +0800

    Import the classes for the routes

--- src/Routes.php
+++ src/Routes.php
@@ -2,6 +2,8 @@
 
 namespace DcodeGroup\Fileman;
 
+use DcodeGroup\Fileman\Http\Controllers\FileController;
+use DcodeGroup\Fileman\Http\Controllers\FolderController;
 use Illuminate\Support\Facades\Route;
 
 class Routes
@@ -10,27 +12,26 @@ class Routes
     {
         Route::group([
             'as' => 'fileman.',
-            'namespace' => '\DcodeGroup\Fileman\Http\Controllers',
         ], function () {
             /*
              * Files
              */
-            Route::post('folder/{parent}/file', 'FileController@store')->name('file.store');
-            Route::get('folder/{parent}/file/create', 'FileController@create')->name('file.create');
-            Route::get('folder/{parent}/file/{file}', 'FileController@show')->name('file.show');
-            Route::put('folder/{parent}/file/{file}', 'FileController@update')->name('file.update');
-            Route::delete('folder/{parent}/file/{file}', 'FileController@destroy')->name('file.destroy');
-            Route::get('folder/{parent}/file/{file}/edit', 'FileController@edit')->name('file.edit');
+            Route::post('folder/{parent}/file', [FileController::class, 'store'])->name('file.store');
+            Route::get('folder/{parent}/file/create', [FileController::class, 'create'])->name('file.create');
+            Route::get('folder/{parent}/file/{file}', [FileController::class, 'show'])->name('file.show');
+            Route::put('folder/{parent}/file/{file}', [FileController::class, 'update'])->name('file.update');
+            Route::delete('folder/{parent}/file/{file}', [FileController::class, 'destroy'])->name('file.destroy');
+            Route::get('folder/{parent}/file/{file}/edit', [FileController::class, 'edit'])->name('file.edit');
 
             /*
              * Folders
              */
-            Route::post('folder/{parent}', 'FolderController@store')->name('folder.store');
-            Route::get('folder/{parent}/create', 'FolderController@create')->name('folder.create');
-            Route::put('folder/{parent}/{folder}', 'FolderController@update')->name('folder.update');
-            Route::delete('folder/{parent}/{folder}', 'FolderController@destroy')->name('folder.destroy');
-            Route::get('folder/{parent}/{folder}/edit', 'FolderController@edit')->name('folder.edit');
-            Route::get('folder/{folder?}', 'FolderController@index')->name('folder.index');
+            Route::post('folder/{parent}', [FolderController::class, 'store'])->name('folder.store');
+            Route::get('folder/{parent}/create', [FolderController::class, 'create'])->name('folder.create');
+            Route::put('folder/{parent}/{folder}', [FolderController::class, 'update'])->name('folder.update');
+            Route::delete('folder/{parent}/{folder}', [FolderController::class, 'destroy'])->name('folder.destroy');
+            Route::get('folder/{parent}/{folder}/edit', [FolderController::class, 'edit'])->name('folder.edit');
+            Route::get('folder/{folder?}', [FolderController::class, 'index'])->name('folder.index');
         });
     }
 }
