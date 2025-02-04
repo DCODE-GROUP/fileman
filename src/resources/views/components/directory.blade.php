d5d165f8806fe884b2a2ae0b2cdab44c5771affd
Gavindecode <gavin@dcodegroup.com>
2025-02-05 09:49:50 +1100

    Add ESLint setup and UI improvements for Fileman package
    Introduce ESLint configuration with rules and ignore files to enhance code quality. Update various views to improve UI consistency and multilingual support. Add `FileService` methods for file counting and initialize front-end integration with Vue.

--- src/resources/views/components/directory.blade.php
+++ src/resources/views/components/directory.blade.php
@@ -1,19 +1,2 @@
 <directory-list :directory="{{json_encode($directory)}}" :folder="{{$folder?->id ?? null}}">
-
 </directory-list>
-
-{{--<a href="{{ isset($directory['url']) ? $directory['url'] : '' }}">--}}
-{{--    <i class="far fa-folder"></i>--}}
-{{--    <span class="text-sm font-semibold text-gray-700">{{ isset($directory['name']) ? $directory['name'] : '' }} </span>--}}
-{{--</a>--}}
-{{--@if (isset($directory['children']) && count($directory['children']) > 0)--}}
-{{--    <ul>--}}
-{{--        @foreach ($directory['children'] as $child)--}}
-{{--            <li>--}}
-{{--                @include('fileman::components.directory', [--}}
-{{--                    'directory' => $child,--}}
-{{--                ])--}}
-{{--            </li>--}}
-{{--        @endforeach--}}
-{{--    </ul>--}}
-{{--@endif--}}
