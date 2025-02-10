4bd57113afe140aab77890fce04292fecaa2322c
Gavindecode <gavin@dcodegroup.com>
2025-02-10 13:45:42 +1100

    Refactor SearchController to extend BaseController
    Updated SearchController to extend BaseController instead of a custom Controller. Removed unnecessary fallback logic for the `$parent` variable, simplifying the code and improving readability.

--- src/Http/Controllers/Api/SearchController.php
+++ src/Http/Controllers/Api/SearchController.php
@@ -2,12 +2,12 @@
 
 namespace DcodeGroup\Fileman\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use DcodeGroup\Fileman\Models\Folder;
 use DcodeGroup\Fileman\Services\SearchService;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Routing\Controller as BaseController;
 
-class SearchController extends Controller
+class SearchController extends BaseController
 {
 
     public function __construct(protected SearchService $searchService)
@@ -16,9 +16,6 @@ class SearchController extends Controller
 
     public function __invoke(Folder $parent)
     {
-        if (!$parent) {
-            $parent = Folder::getRoot();
-        }
         return new JsonResource(
             $this->searchService->search(request()->input('keyword'), $parent->id)
         );
