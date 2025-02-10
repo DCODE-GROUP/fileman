0836ae5e118eb4f508fdced3eaa8a48fa82cf319
Nathan Rzepecki <nathan@lionslair.net.au>
2025-02-10 08:46:03 +0800

    Fix formatting

--- src/Http/Rules/ValidFileName.php
+++ src/Http/Rules/ValidFileName.php
@@ -15,7 +15,6 @@ class ValidFileName implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-
         if (! resolve(FileService::class)->isValidFileNames($value)) {
             $fail(__('fileman.words.invalid_file_name'));
         }
