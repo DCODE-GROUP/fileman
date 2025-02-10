04d9d1c9614231a9d1a1a4c432b5ec5102579561
Gavindecode <gavin@dcodegroup.com>
2025-02-10 17:07:48 +1100

    Refactor route definitions into organized classes.
    Grouped web and API route definitions into dedicated classes for better structure and maintainability. Updated both Web.php and Api.php to encapsulate routes within static methods using `Route::group`.

--- src/Routes/Api.php
+++ src/Routes/Api.php
@@ -7,11 +7,21 @@ use DcodeGroup\Fileman\Http\Controllers\Api\FolderController;
 use DcodeGroup\Fileman\Http\Controllers\Api\SearchController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::put('folder/{parent}/file/{file}', [FileController::class, 'update'])->name('file.update');
-Route::get('folder/{parent}/search', SearchController::class)->name('file.search');
-Route::delete('folder/{parent}/file/{file}', [FileController::class, 'destroy'])->name('file.destroy');
-// folders
-Route::post('folder/{parent}/folder', [FolderController::class, 'store'])->name('folder.store');
-Route::put('folder/{folder}', [FolderController::class, 'update'])->name('folder.update');
-Route::delete('folder/{folder}', [FolderController::class, 'destroy'])->name('folder.destroy');
+class Api
+{
+    public static function get(): void
+    {
+        Route::group([
+            'as' => 'api.fileman.',
+        ], function () {
+            // files
+            Route::put('folder/{parent}/file/{file}', [FileController::class, 'update'])->name('file.update');
+            Route::get('folder/{parent}/search', SearchController::class)->name('file.search');
+            Route::delete('folder/{parent}/file/{file}', [FileController::class, 'destroy'])->name('file.destroy');
+            // folders
+            Route::post('folder/{parent}/folder', [FolderController::class, 'store'])->name('folder.store');
+            Route::put('folder/{folder}', [FolderController::class, 'update'])->name('folder.update');
+            Route::delete('folder/{folder}', [FolderController::class, 'destroy'])->name('folder.destroy');
+        });
+    }
+}
