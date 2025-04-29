9b28ef3c9f00bf521ae8807ceea1a5667b031808
GavinLi312 <salamender336@gmail.com>
2025-04-29 12:43:25 +1000

    Refactor `import` method for recursive folder processing
    Revised the `import` method to handle folder recursion properly, allowing nested folder imports. Introduced optional parameters for parent folder and folder name, improving flexibility and reducing redundancy in folder processing. Additionally, modified file metadata retrieval to enhance clarity and functionality.

--- src/Services/FilemanService.php
+++ src/Services/FilemanService.php
@@ -3,42 +3,41 @@
 namespace DcodeGroup\Fileman\Services;
 
 use DcodeGroup\Fileman\Models\Folder;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class FilemanService
 {
-    public static function import(): void
+    public static function import($parentFolder=null,$folderName = null): void
     {
-        $folderPaths = FileService::getDisk()->allDirectories();
-
-        $root = Folder::firstOrCreate([
-            'parent_id' => null,
-            'name' => '_root',
-        ]);
-
+        $folderPaths = FileService::getDisk()->directories($folderName ?? '');
+        if($parentFolder == null){
+            $parent =  Folder::firstOrCreate([
+                'parent_id' => null,
+            ],[
+                'parent_id' => null,
+                'name' => 'Root',
+            ]);
+        }else{
+            $parent = $parentFolder;
+        }
         foreach ($folderPaths as $folderPath) {
             $folderNames = explode('/', $folderPath);
-
             $folder = Folder::firstOrCreate([
-                'parent_id' => $root->id,
-                'name' => $folderNames[0],
+                'parent_id' => $parent->id,
+                'name' => $folderNames[count($folderNames) - 1],
             ]);
-
-            foreach ($folderNames as $index => $folderName) {
-                if ($index !== 0) {
-                    $folder = Folder::firstOrCreate([
-                        'parent_id' => $folder->id,
-                        'name' => $folderName,
-                    ]);
-                }
-
-                $filePaths = FileService::getDisk()->files($folderPath);
-
-                foreach ($filePaths as $filePath) {
-                    // @phpstan-ignore-next-line
-                    FileService::newFileFromS3($folder, Storage::getMetaData($filePath));
-                }
-            }
+            self::import($folder,$folderPath);
+        }
+        $filePaths = FileService::getDisk()->files($folderName?? '');
+        foreach ($filePaths as $filePath) {
+            // @phpstan-ignore-next-line
+            FileService::newFileFromS3($parent, [
+                'filename' => basename($filePath),
+                'path' => $filePath,
+                'mimetype' => Storage::disk('s3')->mimeType($filePath),
+                'size' => Storage::disk('s3')->size($filePath),
+            ]);
         }
     }
 }
